4d9f6e99beb5dc79925892e367827a64245b698e
b-swist <bswist@protonmail.com>
2026-02-24 19:51:43 +0100

    update completions

--- completions/runny.bash
+++ completions/runny.bash
@@ -1,22 +1,15 @@
 #!/usr/bin/env bash
 
 _runny_comp() {
-	local cword cur prev
+	local cword cur
 	cword="$COMP_CWORD"
 	cur="${COMP_WORDS[cword]}"
-	prev="${COMP_WORDS[cword - 1]}"
 
-	local flags modes
-	flags="-v -h -m"
-	modes="apps path"
+	local flags
+	flags="-v -h"
 
 	if [ $cword -eq 1 ]; then
 		COMPREPLY=($(compgen -W "${flags}" -- "${cur}"))
-		return 0
-	fi
-
-	if [ "$prev" = "-m" ] && [ $cword -eq 2 ]; then
-		COMPREPLY=($(compgen -W "$modes" -- "$cur"))
 		return 0
 	fi
 
